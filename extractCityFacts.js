bc465b7e8fa3ecec1695583a176bb47d6f8cea13
Hamza Amer <hamer1563@gmail.com>
2019-07-09 07:29:08 -0400

    Update extractCityFacts.js

--- extractCityFacts.js
+++ extractCityFacts.js
@@ -1,7 +1,7 @@
 
 //Create a function that takes an object as an argument and returns a string with facts about the city.
 // The city facts will need to be extracted from the object's three properties:
-
+// https://edabit.com/challenge/FzDAgSR84zeyRa278
 function cityFacts(city) {
 let tempV = city.name.toString() + " has a population of " + city.population.toString() +
 		" and is situated in " + city.continent.toString();
